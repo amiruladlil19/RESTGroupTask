e1400e9bc9bdc21f5994431bc11232bc02a3b75e
Abiyyu Ramadhan <edseljobs@gmail.com>
2021-02-07 01:40:35 +0700

    Fix to id, P.S. still not done need sleep.

--- route/itemRoute.js
+++ route/itemRoute.js
@@ -14,28 +14,16 @@ app.get('/item/', (req, res) => {
 app.get('/item/:id', (req, res) => {
     const id = findId(db, req.params.id)
     id ? res.status(200).send(id) : res.status(404).send('item does not exist')
-    //     const id = parseInt(req.params.id, 10);
-    //     db.map((item) => {
-    //         if (item.id === id) {
-    //             return res.status(200).send({
-    //                 item,
-    //             });
-    //         }
-    //     });
-    //     return res.status(404).send({
-    //         error: 'false',
-    //         message: 'item does not exist',
-    //     });
 })
 
-function isIdExist(id, name) {
-    if (db.some(db => db.id == id || db.name == name)) return true;
+function isIdExist(id) {
+    if (db.some(db => db.id == id)) return true;
     else return false;
 }
 
 app.post('/item/', (req, res) => {
-    if (isIdExist(req.body.id, req.body.name)) {
-        res.send('Id or Name is already use')
+    if (isIdExist(req.body.id)) {
+        res.send('Id is already use')
     } else {
         db.push(req.body)
         res.send(req.body)
@@ -45,11 +33,11 @@ app.post('/item/', (req, res) => {
 app.put('/item/:index', (req, res) => {
     const index = req.params.index
     if (!Number(index)) {
-        res.status(400).send('masukkan nomor')
+        res.status(400).send('Input number please')
     } else if ((db.length - 1) < index) {
-        res.status(400).send('item nomor itu gaada')
-    } else if (isIdExist(req.body.id, req.body.name)) {
-        res.send('Id or Name is already use')
+        res.status(400).send('Cannot found number of item')
+    } else if (isIdExist(req.body.id)) {
+        res.send('Id is already use')
     } else {
         db[req.params.index] = req.body
         res.send(req.body)
