cfb51caea141d33fe7fbcb7638629d8bc0fab76f
dwiky <dwiky.darmawansyah@gmail.com>
2021-02-06 14:57:37 +0700

    add user authentication

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,8 +1,16 @@
 const express = require("express");
-// const db = require("../db");
+const db = require("../database/db_user");
 const app = express.Router()
 
+app.post('/register', (req, res) => {
+    db.push(req.body)
+    res.send(req.body)
+})
 
+app.post('/login', (req, res) => {
+    db.push(req.body)
+    res.send(req.body)
+})
 
 
 module.exports = app
