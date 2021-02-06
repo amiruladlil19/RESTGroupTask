0cbb5bab88e762307424f6237e9198055f244fde
Amir <amiruladlilhakim@gmail.com>
2021-02-06 18:43:34 +0700

    editing the route and the db for transaction

---
+++ database/db_transaction.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    id: 1,
+    userId: 1,
+    friendId: 1,
+    itemId: 1,
+    nominal: 50000
+},
+{
+    id: 2,
+    userId: 2,
+    friendId: 2,
+    itemId: 2,
+    nominal: 50000
+}]
--- route/transactionRoute.js
+++ route/transactionRoute.js
@@ -1,7 +1,44 @@
 const express = require("express");
-// const db = require("../db");
+const transactionDB = require("../database/db_transaction");
 const app = express.Router()
 
+
+app.get('/transaction/', (req, res) => {
+    res.send(transactionDB)
+});
+
+app.get('/item/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    db.map((item) => {
+        if (item.id === id) {
+            return res.status(200).send({
+                item,
+            });
+        }
+    });
+    return res.status(404).send({
+        error: 'false',
+        message: 'item does not exist',
+    });
+});
+
+app.put('/transaction/:index', (req, res) => {
+    const index = req.params.index
+    if (!Number(index)) {
+        res.status(400).send('Please input a number')
+    } else if ((transactionDB.length - 1) < index) {
+        res.status(400).send('Out of index')
+    } else {
+        db[req.params.index] = req.body
+        res.send(req.body)
+    }
+});
+
+app.delete('/item/:index', (req, res) => {
+    const index = req.params.index
+    const deletedItem = transactionDB.splice(index, 1);
+    res.send(deletedItem);
+});
 
 
 
