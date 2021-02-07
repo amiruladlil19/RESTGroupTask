475d6a5b2c8a1ca1f76cb7937ffa9387534c7e7d
Abiyyu Ramadhan <edseljobs@gmail.com>
2021-02-07 11:07:41 +0700

    Fix error route

--- route/errorRoute.js
+++ route/errorRoute.js
@@ -5,6 +5,16 @@ app.get('/:path', (req, res) => {
     res.send('Hey miss/sir, we currently cant found what you lookin for')
 })
 
+app.post('/:path', (req, res) => {
+    res.send('Hey miss/sir, we currently cant found what you lookin for')
+})
 
+app.put('/:path', (req, res) => {
+    res.send('Hey miss/sir, we currently cant found what you lookin for')
+})
+
+app.delete('/:path', (req, res) => {
+    res.send('Hey miss/sir, we currently cant found what you lookin for')
+})
 
 module.exports = app
