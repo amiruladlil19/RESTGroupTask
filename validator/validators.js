f6d887b3154e41231e3b2853dd99e865b8c91718
azemoning <ahmadsaufimaulana@gmail.com>
2021-02-07 22:50:55 +0700

    add notempty password to validateregister

--- validator/validators.js
+++ validator/validators.js
@@ -7,6 +7,7 @@ exports.validateRegister = [
     body('username', 'Must a string').isString(),
     body('username', 'Must be minimal 6 characters').isLength({ min: 6 }),
     body('username', 'Must be maximal 12 characters').isLength({ max: 12 }),
+    body('password', 'Cannot be empty').notEmpty(),
     body('password', 'Must be minimal 8 characters').isLength({ min: 8 }),
     body('password', 'Must be maximal 16 characters').isLength({ max: 16 })
 ]
