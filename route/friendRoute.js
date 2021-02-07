bbd45db2e0cbd9a71343b8125353ea08692bc387
azemoning <ahmadsaufimaulana@gmail.com>
2021-02-07 21:39:02 +0700

    implement route friend with validations

--- route/friendRoute.js
+++ route/friendRoute.js
@@ -1,52 +1,75 @@
-
 const express = require("express");
-const db = require("../database/db_friend")
+const { validationResult } = require("express-validator");
+const db_friend = require("../database/db_friend")
+const db_user = require("../database/db_user")
+const fn = require('../function/functions');
+const { validateFriend } = require("../validator/validators");
 const app = express.Router()
 
-function findId(db, itemId) {
-    const result = db.find(({ id }) => id == itemId)
-    return result
-}
-
 app.get('/friend', (req, res) => {
-    res.send(db);
+    res.send(db_friend);
 })
 
 app.get('/friend/:id', (req, res) => {
-    const id = fn.isItemIdExist(db, req.params.id)
-    id ? res.status(200).send(id) : res.status(404).send('friend not found')
-})
-
-app.post('/friend/', (req, res) => {
-    if (fn.isIdExist(db, req.body.id)) {
-        res.send('Id is already use')
-    } else if (fn.isItemDataComplete(req.body) == false) {
-        res.send('Complete the data')
+    const id = fn.isItemIdExist(db_friend, req.params.id)
+    if (id) {
+        res.status(200).send(id)
     } else {
-        db.push(req.body)
-        res.send(req.body)
+        res.status(404).json({
+            status: "404 Not Found",
+            message: "friend not found or does not exist"
+        })
     }
 })
 
-app.put('/friend/:index', (req, res) => {
-    const index = req.params.index
-    if (isNaN(index)) {
-        res.status(400).send('Input number please')
-    } else if ((db.length - 1) < index) {
-        res.status(400).send('Cannot found number of friend')
-    } else if (fn.isIdExist(db, req.body.id)) {
-        res.send('Id is already use')
-    } else if (fn.isItemDataComplete(req.body) == false) {
-        res.send('Complete the data')
+app.post('/friend', validateFriend, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.status(400).json({
+            status: "400 Bad Request",
+            message: errors.array().map(obj => `${obj.param} = ${obj.msg}`)
+        })
     } else {
-        db[req.params.index] = req.body
-        res.send(req.body)
+        if (fn.isIdExist(db_friend, req.body.id)) {
+            res.status(400).json({
+                status: "400 Bad Request",
+                message: "friend id already exist"
+            })
+        } else if (!fn.isIdExist(db_user, req.body.userId)) {
+            res.status(400).json({
+                status: "400 Bad Request",
+                message: "user id does not exist"
+            })
+        } else {
+            db_friend.push(req.body)
+            res.send(req.body)
+        }
     }
 })
 
-app.delete('/friend/:index', (req, res) => {
-    const index = req.params.index
-    const deletedItem = db.splice(index, 1)
+app.put('/friend/:id', validateFriend, (req, res) => {
+    const errors = validationResult(req)
+
+    if (fn.isIdExist(db_friend, req.params.id)) {
+        if (!errors.isEmpty()) {
+            res.status(400).json({
+                status: "400 Bad Request",
+                messages: errors.array().map(obj => `${obj.param} = ${obj.msg}`)
+            })
+        } else {
+            db_friend[fn.findIndexFromId(db_friend, req.params.id)] = req.body
+            res.send(req.body)
+        }
+    } else {
+        res.status(404).json({
+            status: "404 Not Found",
+            message: "friend id not found"
+        })
+    }
+})
+
+app.delete('/friend/:id', (req, res) => {
+    const deletedItem = db_friend.splice(fn.findIndexFromId(db_friend, req.params.id), 1)
     res.send(deletedItem)
 })
 
