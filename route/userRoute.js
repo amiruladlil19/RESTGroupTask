85717c97aecda46fe594b6e10b27f86e6c654791
azemoning <ahmadsaufimaulana@gmail.com>
2021-02-07 18:25:15 +0700

    implements validator on user route

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,34 +1,49 @@
 const express = require("express");
+const { validationResult } = require("express-validator");
 const db_user = require("../database/db_user");
+const fn = require('../function/functions');
+const { validateRegister, validateLogin } = require('../validator/validators')
 const app = express.Router()
 
-function isUserExist(id, username) {
-    if (db_user.some(db_user => db_user.id == id || db_user.username == username)) return true;
-
-    else return false;
-}
-
-app.post('/register', (req, res) => {
-
-    if (isUserExist(req.body.id, req.body.username)) {
-        res.send('UserId or Username is already use')
+app.get('/register', (req, res) => {
+    res.send(db_user)
+})
+app.post('/register', validateRegister, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({
+            status: "400 Bad Request",
+            messages: errors.array().map(msg => `${msg.param} = ${msg.msg}`)
+        })
     } else {
-        db.push(req.body)
-        res.send(req.body)
+        if (fn.isUserExist(db_user, req.body.id, req.body.username)) {
+            res.status(400).json({
+                status: "400 Bad Request",
+                messages: "id or username already exists"
+            })
+        } else {
+            db_user.push(req.body)
+            res.send(req.body)
+        }
     }
 })
 
-function userExists(id, username, password) {
-    return db_user.some(function (el) {
-        return (el.id === id && el.username === username && el.password === password);
-    });
-}
-
-app.post('/login', (req, res) => {
-    if (userExists(req.body.id, req.body.username, req.body.password)) {
-        res.send('login success')
+app.post('/login', validateLogin, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.status(401).json({
+            status: "401 Unauthorized",
+            messages: errors.array().map(msg => `${msg.param} = ${msg.msg}`)
+        })
     } else {
-        res.status(401).send('id, username, or password incorrect')
+        if (fn.isCorrect(db_user, req.body.username, req.body.password)) {
+            res.send('ok')
+        } else {
+            res.status(401).json({
+                status: "401 Unauthorized",
+                messages: "username or password is incorrect"
+            })
+        }
     }
 })
 
