ecb5e0f121f35ca892fcf8703382748d06636304
Abiyyu Ramadhan <edseljobs@gmail.com>
2021-02-07 23:03:25 +0700

    Add validator item

--- validator/validators.js
+++ validator/validators.js
@@ -28,3 +28,14 @@ exports.validateFriend = [
     body('name', 'Must be minimal 3 characters').isLength({ min: 3 }),
     body('name', 'Must be maximal 30 characters').isLength({ max: 30 })
 ]
+
+exports.validateItem = [
+    body('id', 'NaN').isNumeric(),
+    body('id', 'Cannot be empty').notEmpty(),
+    body('userId', 'NaN').isNumeric(),
+    body('userId', 'Cannot be empty').notEmpty(),
+    body('name', 'Must a string').isString(),
+    body('name', 'Cannot be empty').notEmpty(),
+    body('name', 'Must be minimal 3 characters').isLength({ min: 3 }),
+    body('name', 'Must be maximal 30 characters').isLength({ max: 30 })
+]
