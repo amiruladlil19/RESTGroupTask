481fd8bbcb552ec8cdf917572942cea0c4f38007
Abiyyu Ramadhan <edseljobs@gmail.com>
2021-02-07 09:39:57 +0700

    Fix error route

--- route/errorRoute.js
+++ route/errorRoute.js
@@ -5,15 +5,6 @@ app.get('/:path', (req, res) => {
     res.send('Hey miss/sir, we currently cant found what you lookin for')
 })
 
-// app.get('/:path', (req, res) =>{
-//   res.render(req.params.path, {}, (err, html)=>{
-//     if (err){
-//       res.send('404')
-//     }else{
-//       res.send(html)
-//     }
-//   })
-// })
 
 
 module.exports = app
