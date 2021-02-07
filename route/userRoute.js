b752148cb5d4ea0dafbf9cf8db1872f1bb14500f
azemoning <ahmadsaufimaulana@gmail.com>
2021-02-07 20:24:39 +0700

    implements validation on userRoute

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,51 +1,58 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
 const db_user = require("../database/db_user");
-//const db = require("../database/db_user");
 const app = express.Router()
-const exfunction = require("../function/functions")
+const fn = require("../function/functions");
+const { validateRegister, validateLogin } = require("../validator/validators");
 
 app.get('/register', (req, res) => {
     res.send(db_user)
 })
 
-// function isUserExist(id, username) {
-//     if (db_user.some(db_user => db_user.id == id || db_user.username == username)) return true;
-
-//     else return false;
-// }
-
-app.post('/register', (req, res) => {
-
-    if (exfunction.isUserExist(db_user, req.body.id, req.body.username)) {
-        res.send('UserId or Username is already use')
+app.post('/register', validateRegister, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.status(400).json({
+            status: "400 Bad Request",
+            messages: errors.array().map(obj => `${obj.param} = ${obj.msg}`)
+        })
     } else {
-        db_user.push(req.body)
-        res.send(req.body)
+        if (fn.isUserExist(db_user, req.body.id, req.body.username)) {
+            res.status(400).json({
+                status: "400 Bad Request",
+                message: "id or username already exists"
+            })
+        } else {
+            db_user.push(req.body)
+            res.send(req.body)
+        }
     }
 })
 
-// function userExists(id, username, password) {
-//     return db_user.some(function (el) {
-//         return (el.id === id && el.username === username && el.password === password);
-//     });
-// }
-
-// function password(db_user, id, username, password) {
-//     intId = parseInt(id, 10)
-//     const result = db_user.some(user => user.id === intId && user.username == username && user.password != password)
-//     return result
-// }
-
-app.post('/login', (req, res) => {
-    if (exfunction.isCorrect(db_user, req.body.id, req.body.username, req.body.password)) {
-        res.send('login success')
-    } else if (exfunction.isPasswordCorrect(db_user, req.body.id, req.body.username, req.body.password)) {
-        res.status(401).send(`Password incorrect
-        Please enter the correct password`)
+app.post('/login', validateLogin, (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.status(401).json({
+            status: "401 Unauthorized",
+            messages: errors.array().map(obj => `${obj.param} = ${obj.msg}`)
+        })
     } else {
-        res.status(401).send(`id, username, or password incorrect
-        Please register first`)
+        if (fn.isCorrect(db_user, req.body.id, req.body.username, req.body.password)) {
+            res.status(200).json({
+                status: "200 OK",
+                message: "Login succesful"
+            })
+        } else if (fn.isPasswordCorrect(db_user, req.body.id, req.body.username, req.body.password)) {
+            res.status(401).json({
+                status: "401 Unauthorized",
+                message: "Incorrect password"
+            })
+        } else {
+            res.status(401).json({
+                status: "401 Unauthorized",
+                message: "Incorrect username"
+            })
+        }
     }
 })
 
