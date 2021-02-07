a302ded044866ad6f002fbd9048865aa93ace84e
amiruladlil19 <47222829+amiruladlil19@users.noreply.github.com>
2021-02-07 16:25:33 +0700

    Update transactionRoute.js

--- route/transactionRoute.js
+++ route/transactionRoute.js
@@ -1,52 +1,26 @@
 
 const express = require("express");
 const db = require("../database/db_transaction");
+const fn = require('../function/functions');
 const app = express.Router()
+
 
 
 app.get('/transaction/', (req, res) => {
     res.send(db)
-});
+})
 
 app.get('/transaction/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    db.map((transaction) => {
-        if (transaction.id === id) {
-            return res.status(200).send({
-                transaction,
-            });
-        }
-    });
-    return res.status(404).send({
-        error: 'false',
-        message: 'transaction does not exist',
-    });
-});
-
-function isIdExist(id) {
-    if (db.some(db => db.id == id)) return true;
-    else return false;
-}
-
-function isDataComplete(newData) {
-    if ('nominal' in newData
-        && 'itemId' in newData
-        && 'id' in newData
-        && 'userId' in newData
-        && 'friendId' in newData) return true;
-
-    else return false;
-}
-
+    const id = fn.isItemIdExist(db, req.params.id)
+    id ? res.status(200).send(id) : res.status(404).send('transaction does not exist')
+})
 
 app.post('/transaction/', (req, res) => {
-    if (isIdExist(req.body.id)) {
+    if (fn.isIdExist(db, req.body.id)) {
         res.send('Id is already use')
-    }
-    else if (isDataComplete(req.body) == false) {
-        res.send('complete the data')
-    }
-    else {
+    } else if (fn.isDataComplete(req.body) == false) {
+        res.send('Complete the data')
+    } else {
         db.push(req.body)
         res.send(req.body)
     }
@@ -56,15 +30,13 @@ app.put('/transaction/:index', (req, res) => {
     const index = req.params.index
     if (isNaN(index)) {
         res.status(400).send('Input number please')
-    } else if (index < 0 || (db.length - 1) < index) {
-        res.status(400).send('Cannot found the transaction')
-    } else if (isIdExist(req.body.id)) {
+    } else if ((db.length - 1) < index) {
+        res.status(400).send('Cannot found number of transaction')
+    } else if (fn.isIdExist(db, req.body.id)) {
         res.send('Id is already use')
-    }
-    else if (isDataComplete(req.body) == false) {
-        res.send('complete the data')
-    }
-    else {
+    } else if (fn.isDataComplete(req.body) == false) {
+        res.send('Complete the data')
+    } else {
         db[req.params.index] = req.body
         res.send(req.body)
     }
@@ -75,7 +47,6 @@ app.delete('/transaction/:index', (req, res) => {
     const deletedItem = db.splice(index, 1)
     res.send(deletedItem)
 })
-
 
 
 module.exports = app
