1f1f3cd927d8abd23afd291eef32663e76af96cb
Abiyyu Ramadhan <edseljobs@gmail.com>
2021-02-07 11:07:57 +0700

    Fix root route

--- route/rootRoute.js
+++ route/rootRoute.js
@@ -5,5 +5,16 @@ app.get('/', (req, res) => {
     res.send('Welcome to ours API, created by Team no.2')
 })
 
+app.post('/', (req, res) => {
+    res.send('Welcome to ours API, created by Team no.2')
+})
+
+app.put('/', (req, res) => {
+    res.send('Welcome to ours API, created by Team no.2')
+})
+
+app.delete('/', (req, res) => {
+    res.send('Welcome to ours API, created by Team no.2')
+})
 
 module.exports = app
