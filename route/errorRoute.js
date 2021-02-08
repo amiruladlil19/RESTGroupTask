4875e4472ffd83f469adc53ff316b0b17d620e69
azemoning <ahmadsaufimaulana@gmail.com>
2021-02-08 08:02:11 +0700

    update error message on error path

--- route/errorRoute.js
+++ route/errorRoute.js
@@ -2,19 +2,31 @@ const express = require('express')
 const app = express.Router()
 
 app.get('/:path', (req, res) => {
-    res.send('Hey miss/sir, we currently cant found what you lookin for')
+    res.status(404).json({
+        status: "404 Not Found",
+        message: 'we currently cant found what you lookin for'
+    })
 })
 
 app.post('/:path', (req, res) => {
-    res.send('Hey miss/sir, we currently cant found what you lookin for')
+    res.status(404).json({
+        status: "404 Not Found",
+        message: 'we currently cant found what you lookin for'
+    })
 })
 
 app.put('/:path', (req, res) => {
-    res.send('Hey miss/sir, we currently cant found what you lookin for')
+    res.status(404).json({
+        status: "404 Not Found",
+        message: 'we currently cant found what you lookin for'
+    })
 })
 
 app.delete('/:path', (req, res) => {
-    res.send('Hey miss/sir, we currently cant found what you lookin for')
+    res.status(404).json({
+        status: "404 Not Found",
+        message: 'we currently cant found what you lookin for'
+    })
 })
 
 module.exports = app
